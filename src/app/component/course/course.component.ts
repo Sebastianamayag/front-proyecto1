3801147b29b49fc4b0f7dc1be3be86929773b5cc
dlavendano <dl.avendano@uniandes.edu.co>
2021-02-07 15:37:40 -0500

    [style] add carousel images

--- src/app/component/course/course.component.ts
+++ src/app/component/course/course.component.ts
@@ -7,14 +7,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-  peliculas:any[]=[
-    {name:'Frozen 2',
-    img:'https://st3.depositphotos.com/5366154/13715/i/600/depositphotos_137155922-stock-photo-fire-letter-e-of-burning.jpg',
-    desc:'Elsa, Anna, Kristoff and Olaf head far into the forest to learn the truth about an ancient mystery of their kingdom.'},
+  cursos:any[]=[
     {
-      name:'The Irishman',
-      img:'https://s1.1zoom.me/big3/471/Painting_Art_Back_view_Photographer_575380_3840x2400.jpg',
-      desc:'Pennsylvania, 1956. Frank Sheeran, a war veteran of Irish origin who works as a truck driver, accidentally meets mobster Russell Bufalino. Once Frank becomes his trusted man, Bufalino sends him to Chicago with the task of helping Jimmy Hoffa, a powerful union leader related to organized crime, with whom Frank will maintain a close friendship for nearly twenty years.'
+    img:'../../../assets/contenidoGuitarra1.jpg'
+    },
+    {
+      img:'../../../assets/contenidoGuitarra2.jpg'
+    },
+    {
+      img:'../../../assets/contenidoGuitarra3.jpg',
     }
   ];
 
